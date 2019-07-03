a85ec3f3c2afd49c1291306e0233c69790fdc39f
Jacob <jabcog@hotmail.com>
2019-07-03 12:42:14 +1200

    Stuff

---
+++ Assets/Import/ShipsProj/ShipBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShipBehaviour : MonoBehaviour {
+
+    public Vector3 goal;
+    public MeshFilter mf;
+    private int vert;
+    public GameObject sphere;
+
+    private NavMeshAgent me;
+    public float minSpeed = 5;
+    public float maxSpeed = 40;
+
+
+    //get a vertex
+    //get it's position
+    //set transform to position
+
+
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<NavMeshAgent>();
+        vert = Random.Range(1, mf.mesh.vertexCount);
+        goal = mf.mesh.vertices[vert];
+        me.SetDestination(goal);
+        if (sphere != null)
+        {
+            sphere.transform.position = goal;
+        }
+        me.speed = Random.Range(minSpeed, maxSpeed);
+;	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Import/ShipsProj/TinyShipBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TinyShipBehaviour : MonoBehaviour {
+
+    public Vector3 goal;
+    public MeshFilter mf;
+    private int vert;
+
+    private NavMeshAgent me;
+
+
+    //get a vertex
+    //get it's position
+    //set transform to position
+
+
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<NavMeshAgent>();
+        vert = Random.Range(1, mf.mesh.vertexCount);
+        goal = mf.mesh.vertices[vert];
+        me.SetDestination(goal);
+        me.height = Random.Range(1, 6);
+
+;	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
