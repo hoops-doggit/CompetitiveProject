d6f50884ef97441d855c61c3f4023847b001bcdc
Unknown <jabcog@hotmail.com>
2019-08-04 19:42:15 +1200

    Changes
    Fixed GunControl: it had some weird error after merging back
    Changed Post Processing
    Added Post Processing folder

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GunControl : MonoBehaviour {
     private Gun gun;
