3a170da168c976d2e94431598dc312d2c6774aff
hoopsDoggit <jabcog@hotmail.com>
2019-09-12 17:27:49 +1200

    Added scriptable objects from video
    https://www.youtube.com/watch?v=SmnjRCa_iO4
    https://www.youtube.com/watch?time_continue=1054&v=raQ3iHhE_Kk

---
+++ Assets/_Scripts/TestSystems/GameEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+    public void RegisterListener(GameEventListener listener) { }
+    public void UnregisterListener(GameEventListener listener) { }
+
+}
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent Event;
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+
+}
---
+++ Assets/_Scripts/TestSystems/ScriptableObjectTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatVariable : ScriptableObject
+{
+    public float Value;
+
+}
+
+public class DumbEnemy : MonoBehaviour
+{
+    public FloatVariable MaxHP;
+    public FloatVariable MoveSpeed;
+}
+
+[Serializable]
+public class FloatReference
+{
+    public bool UseConstant = true;
+    public float ConstantValue;
+    public FloatVariable Variable;
+
+    public float Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+    }
+}
+
+public class DumbEnemy2 : MonoBehaviour
+{
+    public FloatReference MaxHP;
+    public FloatReference MoveSpeed;
+}
+
