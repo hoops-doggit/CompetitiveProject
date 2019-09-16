d0423266170e3e3a249dd3c99b820d5a228b950e
hoopsDoggit <jabcog@hotmail.com>
2019-09-16 13:27:46 +1200

    Restoring PositionAutoCorrect

---
+++ Assets/_Scripts/Character/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    float autoCorrectDistance = 0.23f;
+    float autoCorrectAmount = 1.01f;
+
+    void PositionAutoCorrect(float front, float back, float left, float right, Vector2 rawinputVector)
+    {
+        //if player is too close to a wall, push them away a little till they're not too close
+        float f = front - transform.position.z;
+        //float b = transform.position
+        float l = left + transform.position.x;
+        float r = right - transform.position.x;
+
+        //front
+        if (f <= autoCorrectDistance && rawinputVector.y == 0)
+        {
+            transform.position -= new Vector3(0, 0, autoCorrectAmount) * Time.deltaTime;
+        }
+        //back
+        if (transform.position.z <= (back + autoCorrectDistance) && rawinputVector.y == 0)
+        {
+            transform.position += new Vector3(0, 0, autoCorrectAmount) * Time.deltaTime;
+        }
+        //left
+        if (transform.position.x <= (left + autoCorrectDistance) && rawinputVector.x == 0)
+        {
+            transform.position += new Vector3(autoCorrectAmount, 0, 0) * Time.deltaTime;
+        }
+        if (r <= autoCorrectDistance && rawinputVector.x == 0)
+        {
+            transform.position -= new Vector3(autoCorrectAmount, 0, 0) * Time.deltaTime;
+        }
+    }
+}
