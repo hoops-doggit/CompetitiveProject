970768b620cbbfe0884c38530d6ae4670c71809a
Jacob <jabcog@hotmail.com>
2019-10-22 12:08:49 +1300

    this

---
+++ Assets/_Scripts/UI/MenuAsset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuAsset : MonoBehaviour
+{
+    public bool selected;
+    public int position;
+    public TextMeshPro text;
+    private string textText;
+    private int timer;
+    //menu assets have 
+    //visual representation eg text/image
+    //bool - selected or not
+    //int - 
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+        textText = text.text;
+    }
+
+
+    public void Selected()
+    {
+        timer++;
+        if (timer > 5)
+        {
+            if (text.text != "")
+            {
+                text.text = "";
+            }
+            else
+            {
+                text.text = textText;
+            }
+            timer = 0;
+        }
+    }
+}
