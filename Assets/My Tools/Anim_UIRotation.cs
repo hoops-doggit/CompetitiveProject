87058d47b45281f4a7299d384b86a7a54b8cb357
Jacob <jabcog@hotmail.com>
2019-11-04 12:30:11 +1300

    UI scene progress

---
+++ Assets/My Tools/Anim_UIRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim_UIRotation : MonoBehaviour
+{
+    private float startAngle;
+    private float endAngle;
+    public AnimationCurve rotation;
+    public enum Axis { x, y, z};
+    public Axis axis;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
